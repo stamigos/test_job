839ce8103720cae877269c5a0e5e253c8da7588b
stamigos <oldtigersvoice@gmail.com>
2015-02-16 07:37:14 +0200

    edited forms

--- library/forms.py
+++ library/forms.py
@@ -1,11 +1,5 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, BooleanField
-from wtforms.validators import Required
-
-
-class LoginForm(Form):
-    openid = TextField('openid', validators=[Required()])
-    remember_me = BooleanField('remember_me', default=False)
+from wtforms import TextField
 
 
 class SearchForm(Form):
