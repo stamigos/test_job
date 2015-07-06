0298f4b12c2ce7125e603f80e5b5fb9693796ba8
stamigos <oldtigersvoice@gmail.com>
2015-07-06 11:06:44 +0300

    test

--- library/views.py
+++ library/views.py
@@ -4,6 +4,7 @@ from library import app
 
 shop_page = Blueprint('shop', __name__, template_folder='templates')
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -40,9 +41,57 @@ def successful():
 
 @app.route('/fail')
 def fail():
-    return render_template('fail.html')
+    ik_co_id = request.args['ik_co_id']
+    ik_inv_id = request.args['ik_inv_id']
+    ik_inv_st = request.args['ik_inv_st']
+    ik_inv_crt = request.args['ik_inv_crt']
+    ik_inv_prc = request.args['ik_inv_prc']
+    ik_pm_no = request.args['ik_pm_no']
+    ik_pw_via = request.args['ik_pw_via']
+    ik_am = request.args['ik_am']
+    ik_co_rfn = request.args['ik_co_rfn']
+    ik_ps_price = request.args['ik_ps_price']
+    ik_cur = request.args['ik_cur']
+    ik_desc = request.args['ik_desc']
+    return render_template('fail.html', params=dict(ik_co_id=ik_co_id,
+                           ik_inv_id=ik_inv_id,
+                           ik_inv_st=ik_inv_st,
+                           ik_inv_crt=ik_inv_crt,
+                           ik_inv_prc=ik_inv_prc,
+                           ik_pm_no=ik_pm_no,
+                           ik_pw_via=ik_pw_via,
+                           ik_am=ik_am,
+                           ik_co_rfn=ik_co_rfn,
+                           ik_ps_price=ik_ps_price,
+                           ik_cur=ik_cur,
+                           ik_desc=ik_desc)
+                           )
 
 
 @app.route('/pending')
 def pending():
-    return render_template('pending.html')
+    ik_co_id = request.args['ik_co_id']
+    ik_inv_id = request.args['ik_inv_id']
+    ik_inv_st = request.args['ik_inv_st']
+    ik_inv_crt = request.args['ik_inv_crt']
+    ik_inv_prc = request.args['ik_inv_prc']
+    ik_pm_no = request.args['ik_pm_no']
+    ik_pw_via = request.args['ik_pw_via']
+    ik_am = request.args['ik_am']
+    ik_co_rfn = request.args['ik_co_rfn']
+    ik_ps_price = request.args['ik_ps_price']
+    ik_cur = request.args['ik_cur']
+    ik_desc = request.args['ik_desc']
+    return render_template('pending.html', params=dict(ik_co_id=ik_co_id,
+                           ik_inv_id=ik_inv_id,
+                           ik_inv_st=ik_inv_st,
+                           ik_inv_crt=ik_inv_crt,
+                           ik_inv_prc=ik_inv_prc,
+                           ik_pm_no=ik_pm_no,
+                           ik_pw_via=ik_pw_via,
+                           ik_am=ik_am,
+                           ik_co_rfn=ik_co_rfn,
+                           ik_ps_price=ik_ps_price,
+                           ik_cur=ik_cur,
+                           ik_desc=ik_desc)
+                           )
