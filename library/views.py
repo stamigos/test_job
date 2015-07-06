c025a64d7d04a0787301569617cf3adcd6124e82
stamigos <oldtigersvoice@gmail.com>
2015-07-06 11:03:16 +0300

    test

--- library/views.py
+++ library/views.py
@@ -1,5 +1,5 @@
 __author__ = 'amigos'
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
 from library import app
 
 shop_page = Blueprint('shop', __name__, template_folder='templates')
@@ -11,7 +11,31 @@ def index():
 
 @app.route('/successful')
 def successful():
-    return render_template('successful.html')
+    ik_co_id = request.args['ik_co_id']
+    ik_inv_id = request.args['ik_inv_id']
+    ik_inv_st = request.args['ik_inv_st']
+    ik_inv_crt = request.args['ik_inv_crt']
+    ik_inv_prc = request.args['ik_inv_prc']
+    ik_pm_no = request.args['ik_pm_no']
+    ik_pw_via = request.args['ik_pw_via']
+    ik_am = request.args['ik_am']
+    ik_co_rfn = request.args['ik_co_rfn']
+    ik_ps_price = request.args['ik_ps_price']
+    ik_cur = request.args['ik_cur']
+    ik_desc = request.args['ik_desc']
+    return render_template('successful.html', params=dict(ik_co_id=ik_co_id,
+                           ik_inv_id=ik_inv_id,
+                           ik_inv_st=ik_inv_st,
+                           ik_inv_crt=ik_inv_crt,
+                           ik_inv_prc=ik_inv_prc,
+                           ik_pm_no=ik_pm_no,
+                           ik_pw_via=ik_pw_via,
+                           ik_am=ik_am,
+                           ik_co_rfn=ik_co_rfn,
+                           ik_ps_price=ik_ps_price,
+                           ik_cur=ik_cur,
+                           ik_desc=ik_desc)
+                           )
 
 
 @app.route('/fail')
