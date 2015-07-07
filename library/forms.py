c6a89c45cd8df6c8566f286d7fb99f0db51520c5
stamigos <oldtigersvoice@gmail.com>
2015-07-07 12:19:55 +0300

    test

--- library/forms.py
+++ library/forms.py
@@ -4,14 +4,23 @@ __author__ = 'amigos'
 from wtforms import Form, BooleanField, TextField, HiddenField, validators
 
 
-class PaymentForm(Form):
+class PaymentFormIntercassa(Form):
     ik_co_id = HiddenField('ik_co_id', default="55964b5d3b1eaf522d8b456c")  # Идентификатор кассы
     ik_pm_no = HiddenField('ik_pm_no', default="ID_4233")  # Номер платежа
     ik_am = HiddenField('ik_am', default="1.44")  # Сумма платежа
    # ik_curr = HiddenField('ik_curr', default="u")
     ik_desc = HiddenField('ik_desc', default="Payment Description")  # Описание платежа
-    ik_int = HiddenField('ik_int', default="web")  # Формат ответа (web / json)
+    ik_int = HiddenField('ik_int', default="json")  # Формат ответа (web / json)
     ik_loc = HiddenField('ik_loc', default="en")  # Локаль
     ik_enc = HiddenField('ik_enc', default="utf-8")  # Кодировка
     ik_act = HiddenField('ik_act', default="process")  # Тип действия (например: payway – способ оплаты)
     ik_pw_via = HiddenField('ik_pw_via', default="w1_w1_merchant_rub")  # Платежная система (например: webmoney_webmoney_merchant_wmz)
+
+
+class PaymentFormWallet(Form):
+    WMI_MERCHANT_ID = HiddenField('WMI_MERCHANT_ID', default="123456789012")
+    WMI_PAYMENT_AMOUNT = HiddenField('WMI_PAYMENT_AMOUNT', default="100.00")
+    WMI_CURRENCY_ID = HiddenField('WMI_CURRENCY_ID', default="643")
+    WMI_DESCRIPTION = HiddenField('WMI_DESCRIPTION', default=u'Оплата демонстрационного заказа')
+    WMI_SUCCESS_URL = HiddenField('WMI_SUCCESS_URL')
+    WMI_FAIL_URL = HiddenField('WMI_FAIL_URL')
--- library/views.py
+++ library/views.py
@@ -1,7 +1,7 @@
 __author__ = 'amigos'
 from flask import render_template, Blueprint, request, json
 from library import app
-from library.forms import PaymentForm
+from library.forms import PaymentFormIntercassa
 import requests
 
 shop_page = Blueprint('shop', __name__, template_folder='templates')
@@ -9,13 +9,18 @@ shop_page = Blueprint('shop', __name__, template_folder='templates')
 
 @app.route('/')
 def index():
-    form = PaymentForm()
+    form = PaymentFormIntercassa()
     d = {'ik_co_id': form.ik_co_id._value(), 'ik_pm_no': form.ik_pm_no._value(), 'ik_am': form.ik_am._value(),
          'ik_int': form.ik_int._value(), 'ik_loc': form.ik_loc._value(), 'ik_enc': form.ik_enc._value(),
          'ik_act': form.ik_act._value(), 'ik_pw_via': form.ik_pw_via._value(), 'ik_desc': form.ik_desc._value()}
     print d
 
-    #r = requests.post("https://sci.interkassa.com/", data=d)
+    r = requests.post("https://sci.interkassa.com/", data=d)
+    r1 = requests.post("https://www.walletone.com/checkout/default.aspx", data={u'paymentForm': {u'action': u'https://www.walletone.com/checkout/default.aspx', u'method': u'post', u'parameters': {u'WMI_FAIL_URL': u'https://sci.interkassa.com/paysystem/return/ps/w1/in/merchant2/ci/38028870/act/failure', u'WMI_CURRENCY_ID': 643, u'WMI_PAYMENT_AMOUNT': u'4.25', u'WMI_PTENABLED': u'WalletOneRUB', u'WMI_PAYMENT_NO': u'38028870', u'WMI_SUCCESS_URL': u'https://sci.interkassa.com/paysystem/return/ps/w1/in/merchant2/ci/38028870/act/success', u'WMI_DESCRIPTION': u'BASE64:UGF5bWVudCBOby4gSUszODAyODg3MA==', u'WMI_SIGNATURE': u'Ix+luoZifqptHVCCw36aow==', u'WMI_MERCHANT_ID': u'122651507690', u'WMI_EXPIRED_DATE': u'2015-08-06 09:04:39'}}})
+    print "r1="
+    print r1.content
+    print "r="
+    print r.json()
     #resp = r.text
     #print resp
     return render_template('index.html', form=form)
