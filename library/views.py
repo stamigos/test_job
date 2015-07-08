ebc8919a7084b4c1a98ade92d53eeb0c4476e03f
stamigos <oldtigersvoice@gmail.com>
2015-07-08 23:46:38 +0300

    test

--- library/views.py
+++ library/views.py
@@ -35,21 +35,27 @@ def index():
     return render_template('index.html', form=form, url=url, params=params_d)
 
 
-@app.route('/successful')
+@app.route('/callback')
+def callback():
+    return render_template('callback.html')
+
+
+@app.route('/successful/')
 def successful():
-    ik_co_id = request.args.get['ik_co_id']
-    ik_inv_id = request.args.get['ik_inv_id']
-    ik_inv_st = request.args.get['ik_inv_st']
-    ik_inv_crt = request.args.get['ik_inv_crt']
-    ik_inv_prc = request.args.get['ik_inv_prc']
-    ik_pm_no = request.args.get['ik_pm_no']
-    ik_pw_via = request.args.get['ik_pw_via']
-    ik_am = request.args.get['ik_am']
-    ik_co_rfn = request.args.get['ik_co_rfn']
-    ik_ps_price = request.args.get['ik_ps_price']
-    ik_cur = request.args.get['ik_cur']
-    ik_desc = request.args.get['ik_desc']
-    return render_template('successful.html', ik_co_id=ik_co_id,
+    ik_co_id = request.args.get('ik_co_id')
+    ik_inv_id = request.args.get('ik_inv_id')
+    ik_inv_st = request.args.get('ik_inv_st')
+    ik_inv_crt = request.args.get('ik_inv_crt')
+    ik_inv_prc = request.args.get('ik_inv_prc')
+    ik_pm_no = request.args.get('ik_pm_no')
+    ik_pw_via = request.args.get('ik_pw_via')
+    ik_am = request.args.get('ik_am')
+    ik_co_rfn = request.args.get('ik_co_rfn')
+    ik_ps_price = request.args.get('ik_ps_price')
+    ik_cur = request.args.get('ik_cur')
+    ik_desc = request.args.get('ik_desc')
+    return render_template('successful.html',
+                           ik_co_id=ik_co_id,
                            ik_inv_id=ik_inv_id,
                            ik_inv_st=ik_inv_st,
                            ik_inv_crt=ik_inv_crt,
@@ -64,20 +70,20 @@ def successful():
                            )
 
 
-@app.route('/fail')
+@app.route('/fail/')
 def fail():
-    ik_co_id = request.args.get['ik_co_id']
-    ik_inv_id = request.args.get['ik_inv_id']
-    ik_inv_st = request.args.get['ik_inv_st']
-    ik_inv_crt = request.args.get['ik_inv_crt']
-    ik_inv_prc = request.args.get['ik_inv_prc']
-    ik_pm_no = request.args.get['ik_pm_no']
-    ik_pw_via = request.args.get['ik_pw_via']
-    ik_am = request.args.get['ik_am']
-    ik_co_rfn = request.args.get['ik_co_rfn']
-    ik_ps_price = request.args.get['ik_ps_price']
-    ik_cur = request.args.get['ik_cur']
-    ik_desc = request.args.get['ik_desc']
+    ik_co_id = request.args.get('ik_co_id')
+    ik_inv_id = request.args.get('ik_inv_id')
+    ik_inv_st = request.args.get('ik_inv_st')
+    ik_inv_crt = request.args.get('ik_inv_crt')
+    ik_inv_prc = request.args.get('ik_inv_prc')
+    ik_pm_no = request.args.get('ik_pm_no')
+    ik_pw_via = request.args.get('ik_pw_via')
+    ik_am = request.args.get('ik_am')
+    ik_co_rfn = request.args.get('ik_co_rfn')
+    ik_ps_price = request.args.get('ik_ps_price')
+    ik_cur = request.args.get('ik_cur')
+    ik_desc = request.args.get('ik_desc')
     return render_template('fail.html', ik_co_id=ik_co_id,
                            ik_inv_id=ik_inv_id,
                            ik_inv_st=ik_inv_st,
@@ -93,20 +99,20 @@ def fail():
                            )
 
 
-@app.route('/pending')
+@app.route('/pending/')
 def pending():
-    ik_co_id = request.args.get['ik_co_id']
-    ik_inv_id = request.args.get['ik_inv_id']
-    ik_inv_st = request.args.get['ik_inv_st']
-    ik_inv_crt = request.args.get['ik_inv_crt']
-    ik_inv_prc = request.args.get['ik_inv_prc']
-    ik_pm_no = request.args.get['ik_pm_no']
-    ik_pw_via = request.args.get['ik_pw_via']
-    ik_am = request.args.get['ik_am']
-    ik_co_rfn = request.args.get['ik_co_rfn']
-    ik_ps_price = request.args.get['ik_ps_price']
-    ik_cur = request.args.get['ik_cur']
-    ik_desc = request.args.get['ik_desc']
+    ik_co_id = request.args.get('ik_co_id')
+    ik_inv_id = request.args.get('ik_inv_id')
+    ik_inv_st = request.args.get('ik_inv_st')
+    ik_inv_crt = request.args.get('ik_inv_crt')
+    ik_inv_prc = request.args.get('ik_inv_prc')
+    ik_pm_no = request.args.get('ik_pm_no')
+    ik_pw_via = request.args.get('ik_pw_via')
+    ik_am = request.args.get('ik_am')
+    ik_co_rfn = request.args.get('ik_co_rfn')
+    ik_ps_price = request.args.get('ik_ps_price')
+    ik_cur = request.args.get('ik_cur')
+    ik_desc = request.args.get('ik_desc')
     return render_template('pending.html', ik_co_id=ik_co_id,
                            ik_inv_id=ik_inv_id,
                            ik_inv_st=ik_inv_st,
